a156be33d4db9d912e6c8a448a375f457b959c07
Francisco Javier Hernandez <oficial_javier_26@hotmail.com>
2019-11-24 19:40:21 -0600

    Se completo la función de editar contacto, ahora funciona

--- src/pages/editar/editar.ts
+++ src/pages/editar/editar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Generated class for the EditarPage page.
@@ -14,14 +15,61 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'editar.html',
 })
 export class EditarPage {
+  correo = "";
+  avatar = "";
+  nombre = "";
+  telefono = "";
+  facebook = "";
+  instagram = "";
+  twitter = "";
+  id = "";
+
   contacto = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
     this.contacto = navParams.get('data');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditarPage');
+  }
+
+  clickAcept(){
+    if (this.correo !== "") {
+      this.contacto['correo'] = this.correo;
+    }
+    if (this.avatar !== "") {
+      this.contacto['avatar'] = this.avatar;
+    }
+    if (this.nombre !== "") {
+      this.contacto['nombre'] = this.nombre;
+    }
+    if (this.telefono !== "") {
+      this.contacto['telefono'] = this.telefono;
+    }
+    if (this.facebook !== "") {
+      this.contacto['facebook'] = this.facebook;
+    }
+    if (this.instagram !== "") {
+      this.contacto['instagram'] = this.instagram;
+    }
+    if (this.twitter !== "") {
+      this.contacto['twitter'] = this.twitter;
+    }
+    this.contacto['editar'] = "SI";
+    const contact = this.contacto;
+    console.log(JSON.stringify(this.contacto));
+
+    this.http.post('/contacto/contacto/', contact).subscribe(data => {console.log(JSON.stringify(data));
+      this.navCtrl.pop();
+    }, error => {console.log(JSON.stringify(error));
+      this.navCtrl.pop();
+    });
+    this.navCtrl.pop();
+  }
+
+  clickDisscard() {
+    this.navCtrl.pop();
   }
 
 }
